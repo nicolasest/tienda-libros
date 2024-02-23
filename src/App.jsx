8dbe5811435f5187fbc1cf3cccb4201e0cebf41f
Nicolas Estallo <30913558+nicolasest@users.noreply.github.com>
2024-02-23 18:37:18 +0000

    change 6 to 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
+import "./App.css";
 
-
-import './App.css'
-
-import { ItemListContainer } from './components/pages/ItemListContainer/ItemListContainer'
-import { NavBar} from './components/layout/NavBar/NavBar.jsx'
-import { ItemCountContainer} from './components/common/ItemCount/ItemCountContainer.jsx'
+import { ItemListContainer } from "./components/pages/ItemListContainer/ItemListContainer";
+import { NavBar } from "./components/layout/NavBar/NavBar.jsx";
+import { ItemCountContainer } from "./components/common/ItemCount/ItemCountContainer.jsx";
 function App() {
-
-
   return (
     <>
-    <NavBar />
-    <ItemListContainer />
-    <ItemCountContainer stock={6} />
+      <NavBar />
+      <ItemListContainer />
+      <ItemCountContainer stock={7} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
